8be657735f760ddeb256fb1b1132c7cd27c8eea0
leo <cedillo47@gmail.com>
2021-11-08 15:25:24 +0000

    conpleated

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //write your code here
+const averageInArray = (num) => {
+ const length = num.length; 
+let total = 0;
+for (let i = 0; i <= num.length - 1; i++){
+total += num[i];
+}
+total  = total / length;
+return total ;
+};
+
+
+console.log(averageInArray([2,2,2,2,2]))
